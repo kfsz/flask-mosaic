1e32cc89a4d16f779fdf1de8057188e4d07fb2f2
kfsz <kfs.szk@gmail.com>
2019-04-19 16:49:15 +0200

    massive trash removal

--- main.py
+++ main.py
@@ -4,61 +4,20 @@ from io import BytesIO, StringIO
 from random import shuffle
 import requests
 import math
-# dunno if re should be used
 import re
 
-# ' to " later
-# https://stackoverflow.com/questions/38421160/combine-multiple-different-images-from-a-directory-into-a-canvas-sized-3x6
-# hmmmmmmmmmmmmmmmmmmmm
 app = Flask(__name__)
 
-# add support for png files! eh, seems to work
-# some resolutions don't work
-
-# dunno if it wouldnt be better to simply randomize and duplicate image if odd number exists
-
-'''
-kay, so only 8 images - maybe should be hardcoded for 'best' positions?
-1 - x
-2 - xx (change if one size bigger?)
-3 - xxx (change if one size bigger?)
-4 - xx
-    xx
-5 - Ixx
-    Ixx
-6 - xxx
-    xxx
-7 - Ixxx
-    Ixxx
-8 - xxxx
-    xxxx
-    
-    
-so odd is //2 or -1
-if? based on switch?
-
-still problem with 7, when same x same
-'''
-
-
-
-def create_img(img_loc, size):
-
+def create_mosaic(img_loc, size):
+    '''Create mosaic using images from given urls and size.'''
     # create empty image
     fill_color = 'black'
     mozaic = Image.new('RGB', size, fill_color)
     
     image_number = len(img_loc)
-    # adjust image number to nearest 2x2 or so
-    # MAX 8 IMAGES SO 4x2 SHOULD BE BIGGEST?
-    
-    # img_loc = img_loc[0]
-    #horizontal_change = 1000
-    #vertical_change = 1000
-    
-    # right, so calculations
-    # change rows and columns naming
     odd_image = False
+    
+    # basic mosaic layout creation
     if image_number >= 3:
         rows = image_number//2
         columns =  2
@@ -68,105 +27,65 @@ def create_img(img_loc, size):
         rows = image_number
         columns =  1
     
-    # switch size
-    
-    # not sure if needed
+    # switch rows with columns (image placement) depending
+    # on wherever is more free space available
     if size[1] > size[0]:
-        print(rows, columns)
         rows, columns = columns, rows
-        print(rows, columns)
-    
-    print(str(rows) + " xxx " + str(columns))
-    # total size is fucked up, as it assumes horizontal == vertical - FIX - to be removed
-    total_size = size[0] * size[1]
-    total_size = total_size/image_number
-    total_size = int(math.sqrt(total_size))
-    horizontal_change = total_size
-    vertical_change = total_size
-    
-    # second part of calculations 
-    # check which one is bigger
+
+    # create image restrictions
     horizontal_change = size[0]//rows
     vertical_change = size[1]//columns
     
-    
-    switch = 0
+    # adjust image restrictions when creating mosaic
+    # with odd number of images
     if odd_image:
         if horizontal_change > vertical_change:
             horizontal_change = size[0]//(rows+1)
             rows += 1
-            switch = 1
         else:
             vertical_change = size[1]//(columns+1)
             columns += 1
-            switch = 2
-    
-    
-    print("horizontal_change " + str(horizontal_change))
-    print("vertical_change " + str(vertical_change))
-    #print(switch)
+  
     current = 0
     current_row = 0
     current_column = 0
-    print("image # is" + str(image_number))
+    
     for i in range(0, size[0], horizontal_change):
         current_row = 0
         current_column += 1
         for j in range(0, size[1], vertical_change):
-            current_row += 1
-            # check if out of bounds - shouldn't be needed - remove - tho, hmm; floats
-            # yea, floats are fucked - tries to show at 0x3999 for 1080x4000
+        
+            # check if out of bounds
             if j + vertical_change > size[1]:
                 break
+                
+            # load and open image
             try:
-                filepath = img_loc.pop(0)
+                file_url = img_loc.pop(0)
             except IndexError:
                 break
-            current += 1 # change that into something nicer
-            response = requests.get(filepath)
+            response = requests.get(file_url)
             img = Image.open(BytesIO(response.content))
-            # hmmmmmmmmmmmmmmmmmmmm / looks better than in 'docs' - with switch
-            print("row" + str(current_row) + " " + str(rows))
-            print("column" + str(current_column) + " " + str(columns))
+
+            current_row += 1            
+            current += 1
+            
+            # resize images based on free space available
             if odd_image and current==image_number and horizontal_change > vertical_change:
-                print('lol, here')
                 img = img.resize((horizontal_change, vertical_change*2), Image.ANTIALIAS)
-            # ceiling division - upside-down floor division - hmmm
-            # gotta fix - needs sth else - not sure if this works 100% !!!!! and current+rows>image_number
-            # something is still fucked up - only when same x same, so <= should be < somewhere
-            #elif horizontal_change <= vertical_change and current_column == rows and current+columns>image_number:
-            #elif horizontal_change <= vertical_change and current_column == columns-1 and current+rows>image_number:
             elif (current_column == rows-1 and current+columns>image_number and
-                 (horizontal_change <= vertical_change or 
-                 # special case for when there is 7 pic mosaic
-                 # with strenched last image and empty space at the bottom
-                 current+columns>image_number+1)):
-                 # case #1 - 2 rows
-                 #(switch == 1 and current==image_number-1) or
-                 # case #2 - 3 rows and 4 rows
-                 #(switch == 2 and current==image_number//2 + 1)):
-                print('lol, there')
+                  (horizontal_change <= vertical_change or
+                   current+columns>image_number+1)): # special case for 7 pic mosaic
+                   # with stretched image and still empty space at the bottom
                 img = img.resize((horizontal_change*2, vertical_change), Image.ANTIALIAS)
-                #odd_image = 0
             else:
                 img = img.resize((horizontal_change, vertical_change), Image.ANTIALIAS)
-            print("current " + str(current) + " at " + str(i) + "x" + str(j))
             mozaic.paste(img, (i,j))
     
     return mozaic
-    '''
-    response = requests.get(img_loc)
-    img = Image.open(BytesIO(response.content))
-    # fill transparent pixels with white for later jpg conversion
-    fill_color = 'white'
-    if img.mode in ('RGBA', 'LA'):
-        background = Image.new(img.mode[:-1], img.size, fill_color)
-        background.paste(img, img.split()[-1])
-        img = background
-    return img
-    '''
     
 def serve_pil_image(pil_img):
+    '''Send image from Flask.'''
     #size = 2048, 2048
     #pil_img = pil_img.resize(size, Image.ANTIALIAS)
     img_io = BytesIO()
@@ -176,14 +95,13 @@ def serve_pil_image(pil_img):
     
 @app.route('/mozaika', methods=['GET'])
 def mosaic():
+    '''Basic endpoint for getting mosaic image.'''
+    # get parameters
     random = request.args.get('losowo')
     size = request.args.get('rozdzielczosc')
     images = request.args.get('zdjecia').split(sep=',')
-    
-    #### RANDOMIZE LIST ORDER IF RANDOM
-    
-    # size processing
-    # dunno, really
+  
+    # size validation and processing
     if size and re.match(r"[1-9][0-9]*x[1-9][0-9]*", size):
         size = size.split(sep='x')
         size = [int(i) for i in size]
@@ -191,15 +109,11 @@ def mosaic():
         size = (2048, 2048)
     
     # randomize
-    # can potentially crash - prob better to str instead
-    if random and int(random)==1:
+    if random and random==str(1):
         shuffle(images)
         
-    print(images)
-    print(len(images))
-    
-    img = create_img(images, size)
-    return serve_pil_image(img)
+    mosaic = create_mosaic(images, size)
+    return serve_pil_image(mosaic)
     
 
 if __name__ == '__main__':
